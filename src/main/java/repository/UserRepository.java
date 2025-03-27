3fdca43097eb2f834130d45ae39f1f2dc037676d
SokolovNetology <sokolov180375@mail.ru>
2025-03-27 16:09:36 +0300

    diplom_cloud_service

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -3,7 +3,7 @@ package repository;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.netology.cloud_service.model.entities.User;
+import model.entities.User;
 
 import java.util.Optional;
 
